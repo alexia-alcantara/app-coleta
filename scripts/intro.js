6685ed013db5381cb054efbc246757673f9ab494
alexia-alcantara <alexialcantara@outlook.com>
2020-06-04 00:05:11 -0300

    comitando ultimas alterações

---
+++ scripts/intro.js
@@ -0,0 +1,88 @@
+// document.write("Hello Word")
+// var texto
+// texto = "Hello Everybody"
+
+// document.write(texto)
+
+// let doc = "Ola"
+// const mydoc = "Pessoal"
+
+// document.write(doc + mydoc)
+
+
+
+
+
+//string
+// const banda1 = 'Foster the People'
+// const banda2 = 'Lady Gaga'
+// const banda3 = 'Strike'
+
+// document.write(banda2)
+
+
+
+
+//objeto
+
+// const carro = {
+//   cor: "vermelho",
+//   ano: 2009,
+//   preço: 25.000,
+  
+//   anda() {
+//     document.write("corra 0km")
+//   }
+// }
+
+// carro.anda()
+
+
+
+
+ //array
+
+// const colecao = ["cd", "tv", "celular"]
+// document.write(colecao[3])
+
+
+
+
+
+//função
+
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// sayMyName("Mel")
+
+
+
+
+
+
+//condicional
+
+// const notaFinal = 10
+
+// if( notaFinal < 5) {
+//    document.write("Reprovado")
+//    } else {
+//      document.write("Aprovado")
+//    }
+
+
+
+
+
+
+
+
+//loop
+
+// for (i= 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>` )
+// }
+
+
